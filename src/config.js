4316cf9e1d5d31ee6f1da3971444c45c1a7ed22a
StephaneCzr <stephane.czr@gmail.com>
2017-04-11 16:45:05 +0200

    Delete config.js

--- src/config.js
+++
@@ -1,5 +0,0 @@
-process.env.REQUEST_TOKEN = '08c09e2bd44dba9f0aa875f60a8ca085'
-process.env.LANGUAGE = 'en'
-process.env.PORT = '5000'
-process.env.GITHUB_TOKEN = '5208948df1022d7989c16b0711b711039150e35d'
-process.env.GITHUB_USERNAME = 'StephaneCzr'
